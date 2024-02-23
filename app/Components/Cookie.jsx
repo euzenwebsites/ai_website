068dd2aef79b589bf9c38ce883c16fbc5d69f2dd
euzenwebsites <152855764+euzenwebsites@users.noreply.github.com>
2024-02-23 20:08:06 +0500

    update

--- app/Components/Cookie.jsx
+++ app/Components/Cookie.jsx
@@ -11,13 +11,13 @@ const Cookie = () => {
         show && <div className="bottom-0 fixed bg-white z-50 w-screen px-4 py-2">
           <div className="flex justify-between items-center text-gray-500">
             <p>
-              We use cookies to ensure you have the best experience on our site, to analyze traffic, and enhance our marketing activities. <a className="text-gray-600 underline">Learn more</a>
+              We use cookies to ensure you have the best experience on our site to analyze traffic, and enhance our marketing activities. <a className="text-gray-600 underline">Learn more</a>
             </p>
             <div className="flex justify-between items-center gap-2">
-              <button className="bg-black text-white px-3 py-2 rounded-xl">
+              <button className="bg-black text-white p-2 rounded-xl">
                 Cookie settings
               </button>
-              <button className="bg-indigo-500 text-white px-3 py-2 rounded-xl" onClick={() => setShow(false)}>
+              <button className="bg-indigo-500 text-white p-2 rounded-xl" onClick={() => setShow(false)}>
                 Accept all Cookies
               </button>
             </div>
