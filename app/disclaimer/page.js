58e74eca8d03fe7772c04ec1f15c6cf76b8283a9
euzenwebsites <152855764+euzenwebsites@users.noreply.github.com>
2024-02-29 04:00:06 +0500

    update

--- app/disclaimer/page.js
+++ app/disclaimer/page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const page = () => (
-    <iframe className='w-screen h-screen' src="https://app.termly.io/document/disclaimer/cee91a12-0f3c-408e-addc-dd5393c97365" title="W3Schools Free Online Web Tutorials"></iframe>
+    <iframe className='w-screen h-screen' src="https://app.termly.io/document/disclaimer/cee91a12-0f3c-408e-addc-dd5393c97365" title="Toolbelts AI Disclaimer"></iframe>
 )
 
 export default page
