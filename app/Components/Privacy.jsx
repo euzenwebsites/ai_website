6756518671b914a2b8d8af28cc43f92d976c1403
euzenwebsites <152855764+euzenwebsites@users.noreply.github.com>
2024-02-29 03:47:32 +0500

    update

--- app/Components/Privacy.jsx
+++ app/Components/Privacy.jsx
@@ -1,21 +1,58 @@
 import React from 'react'
 
-const Privacy = () => (
-    <div className='overflow-x-hidden flex flex-col items-center h-screen md:px-[100px] sm:px-[40px] px-[10px] mt-5'>
-        <div className='max-w-[900px] min-w-[95vw] sm:min-w-[700px] h-screen overflow-x-hidden'>
-            <h1 className='text-3xl font-semibold mb-3'>PRIVACY POLICY</h1>
+{/* const Privacy = () => (
+    <div className='overflow-x-hidden flex flex-col items-center md:px-[100px] sm:px-[40px] px-[10px] mt-10'>
+        <div className='max-w-[800px] min-w-[95vw] sm:min-w-[700px] overflow-x-hidden'>
+            <h1 className='text-2xl font-bold mb-3'>PRIVACY POLICY</h1>
             <p className='font-semibold text-gray-600'>Last updated August 31, 2022</p>
-            <div className='text-gray-600 text-sm'>
-                <p className='mt-10'>This privacy notice fot PRIVACY POLICY Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed quaerat doloribus, minus natus odio reiciendis nulla ab! Perspiciatis, magnam assumenda.</p>
+            <div className='text-gray-600 text-sm mt-10'>
+                <p>This privacy notice for RainMakerAI, LLC ( <span className="font-bold">&ldquo; we, &ldquo;</span> <span className="font-bold">&ldquo; us, &ldquo;</span> <span className="font-bold">&ldquo; our, &ldquo;</span> ), describes how and why we might collect, store, use, and/or share (&ldquo;process&ldquo;) your information when you use our services ( <span className="font-bold">&ldquo; Services, &ldquo;</span>), such as when you:</p>
                 <ul className='mt-5 list-disc ml-9'>
-                    <li className='mb-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, earum. Lorem ipsum dolor sit amet.</li>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, architecto.</li>
+                    <li className='mb-5'>Visit our website at <a href="https://rainmakerai.com" className='text-blue-700 underline' target='_blank'>https://rainmakerai.com</a> , or any website of ours that links to this privacy notice</li>
+                    <li>Engage with us in other related ways, including any sales, marketing, or events</li>
                 </ul>
-                <p className='mt-6'><span className='font-semibold'>Questions or concerns?</span> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, id impedit deserunt nisi nostrum ratione sequi aut et sint cupiditate! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, repellendus. </p>
+                <QnA q="Questions or concerns?" a="Reading this privacy notice will help you understand your privacy rights and choices. If you do not agree with our policies and practices, please do not use our Services. If you still have any questions or concerns, please contact us at support@rainmakerai.com" />
             </div>
-            <h1 className='text-3xl font-semibold my-10'>PRIVACY POLICY</h1>
+            <SubHeading heading={"SUMMARY OF KEY POINTS"} />
+            <div className="text-gray-600 text-sm mt-10">
+                <p className='font-semibold italic'>This summary provides key points from our privacy notice, but you can find out more details about any of these topics by clicking the link following each key point or by using our table of contents below to find the section you are looking for.</p>
+                <QnA q="What personal information do we process?" a="When you visit, use, or navigate our Services, we may process personal information depending on how you interact with us and the Services, the choices you make, and the products and features you use. Learn more about personal information you disclose to us." />
+                <QnA q="Do we process any sensitive personal information? " a="We do not process sensitive personal information." />
+                <QnA q="Do we receive any information from third parties? " a="We do not receive any information from third parties." />
+                <QnA q="How do we process your information? " a=" We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent. We process your information only when we have a valid legal reason to do so. Learn more about how we process your information." />
+                <QnA q="In what situations and with which types of parties do we share personal information? " a=" We may share information in specific situations and with specific categories of third parties. Learn more about when and with whom we share your personal information." />
+                <QnA q="How do we keep your information safe? " a=" We have organizational and technical processes and procedures in place to protect your personal information. However, no electronic transmission over the internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other unauthorized third parties will not be able to defeat our security and improperly collect, access, steal, or modify your information. Learn more about how we keep your information safe." />
+                <QnA q="What are your rights?" a=" Depending on where you are located geographically, the applicable privacy law may mean you have certain rights regarding your personal information. Learn more about your privacy rights. " />
+                <QnA q="How do you exercise your rights?" a="  The easiest way to exercise your rights is by submitting a data subject access request, or by contacting us. We will consider and act upon any request in accordance with applicable data protection laws." />
+                <p className="mt-6">Want to learn more about what we do with any information we collect? Review the privacy notice in full.</p>
+            </div>
+            <SubHeading heading={"TABLE OF CONTENTS"} />
+            <ol className='text-blue-700 underline list-decimal mt-6 font-medium'>
+                {Array("WHAT INFORMATION DO WE COLLECT?", "HOW DO WE PROCESS YOUR INFORMATION?", "WHAT LEGAL BASES DO WE RELY ON TO PROCESS YOUR PERSONAL INFORMATION?", "WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?", "DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?", "IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?", "HOW LONG DO WE KEEP YOUR INFORMATION?", "HOW DO WE KEEP YOUR INFORMATION SAFE?", "DO WE COLLECT INFORMATION FROM MINORS?", "WHAT ARE YOUR PRIVACY RIGHTS?", "CONTROLS FOR DO-NOT-TRACK FEATURES", "DO UNITED STATES RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?", "DO OTHER REGIONS HAVE SPECIFIC PRIVACY RIGHTS?", "DO WE MAKE UPDATES TO THIS NOTICE?", "HOW CAN YOU CONTACT US ABOUT THIS NOTICE?", "HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?").map((li, i) => {
+                    return <li key={i} className='ml-5 mt-2'>{li}</li>
+                })}
+            </ol>
+            <SubHeading heading={"1. WHAT INFORMATION DO WE COLLECT?"} />
+            <h5 className='text-lg font-semibold mt-6'>Personal information you disclose to us</h5>
+            <QnA q={"In Short:"} a={"We collect personal information that you provide to us."} />
         </div>
     </div>
+) */}
+const Privacy = () => (
+    <>
+        <iframe className='w-screen h-screen' src="https://app.termly.io/document/privacy-policy/bf5d0038-76f9-47db-a47d-1d4ebb45365e#infominors" title="W3Schools Free Online Web Tutorials"></iframe>
+    </>
+)
+export default Privacy
+
+/* const Bold = ({ text }) => (
+    <b>{text}</b>
 )
 
-export default Privacy
+const QnA = ({ q, a, customClass }) => (
+    <p className={`mt-6 ${customClass}`}><span className='font-semibold'>{q}</span> {a} </p>
+)
+
+const SubHeading = ({ heading }) => (
+    <h3 className='text-xl font-semibold mt-10'>{heading}</h3>
+) */
