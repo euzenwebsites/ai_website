f7ded1366344a659899a0318acd42c92988f1d19
euzenwebsites <152855764+euzenwebsites@users.noreply.github.com>
2024-03-05 03:26:41 +0500

    update

--- app/Components/GoogleSignInButton.jsx
+++ app/Components/GoogleSignInButton.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
-import { useSearchParams } from 'next/navigation'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
+import axios from '../axios'
 
 const GoogleSignInButton = () => {
-    const searchParams = useSearchParams()
-    const callbackUrl = searchParams.get('callbackUrl')
+    const session = useSession()
+    console.log(session);
+
     return (
-        <button  onClick={() => signIn('google', { callbackUrl })} className="py-4 px-6 border justify-center flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150 sm:w-[400px] w-[80%] mb-0 mt-14">
+        <button type='submit' onClick={() => signIn('google')} className="py-4 px-6 border justify-center flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150 sm:w-[400px] w-[80%] mb-0 mt-14">
             <Image height={24} width={24}
                 className="w-6 h-6"
                 src="https://www.svgrepo.com/show/475656/google-color.svg"
--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,21 +1,13 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId:"147027649761-76uld5m14vh1ij57bn3jrh6hk3o1mem9.apps.googleusercontent.com",
-      // clientId: process.env.GOOGLE_CLIENT_ID,
-      // clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      clientSecret:"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"
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
-  /* pages: {  
-    logIn: '/login',
-    signUp: '/signup'
-  } */
-}
-
-const handler = NextAuth(authOptions)
+})
 
 export { handler as GET, handler as POST }
