d4dfe0eb8ff6b891631a54cb7e51bcff53f01b0e
euzenwebsites <152855764+euzenwebsites@users.noreply.github.com>
2024-03-05 03:35:32 +0500

    update

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import AuthProvider from "./Components/Authprovider/Authprovider.js";
+import AuthProvider from "./Components/Authprovider";
 
 const inter = Inter({ subsets: ["latin"] });
 
